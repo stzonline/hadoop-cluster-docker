bd2cbd856c515e7a80ba4ae3bc7f2a701f795d16
stzonline <stzonline@qq.com>
2016-06-20 17:54:37 +0800

    Update start.sh

--- config/start.sh
+++ config/start.sh
@@ -4,7 +4,8 @@
 #source /etc/profile
 #echo $JAVA_HOME
 
-service sshd start
+#service sshd start
+service ssh start
 
 #echo "172.17.0.3 master" >> /etc/hosts
 #echo "172.17.0.2 slave1" >> /etc/hosts
@@ -14,7 +15,7 @@ $HADOOP_HOME/sbin/start-yarn.sh
 
 $SPARK_HOME/sbin/start-all.sh
 
-jps
+#jps
 
 #tail -10f /home/hadoop/hadoop-2.6.2/etc/hadoop/start.sh
 #tail -100f /opt/hadoop-1.2.1/conf/end.txt
