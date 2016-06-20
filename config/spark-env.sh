f0f20fec6377f644528c37f7377d7b22357ed1fd
stzonline <stzonline@qq.com>
2016-06-20 12:29:58 +0800

    Update spark-env.sh

--- config/spark-env.sh
+++ config/spark-env.sh
@@ -2,6 +2,6 @@ export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_91
 export HADOOP_HOME=/usr/local/hadoop
 export SCALA_HOME=/usr/lib/scala/scala-2.11.8
 export SPARK_HOME=/usr/local/spark
-export SPARK_MASTER_IP=172.17.0.3
+export SPARK_MASTER_IP=master
 export SPARK_WORKER_MEMORY=1g
 export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop
